28a37fc76ca7e27f8db748721952af098d77dac2
Harry Chen <i@harrychen.xyz>
2018-11-09 10:57:54 +0800

    runAll.py: make output directory if not existed

--- TestCases/S2/runAll.py
+++ TestCases/S2/runAll.py
@@ -22,6 +22,8 @@ def read_txt_file(filename):
 
 def main():
     decaf_jar = os.path.join('..', '..', 'result', 'decaf.jar')
+    # in case output dir not exists
+    os.makedirs('output', exist_ok=True)
     names = sys.argv[1:]
     if not names:
         names = sorted(os.listdir('.'))
